c7b93e46c7be4fe92bb9886f92145ae61ef13095
Joe Chang <changjoe01@gmail.com>
2014-01-12 14:46:35 +0800

    Before cart has been implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
-ToyStore::Application.routes.draw do
+ToyStore::Application.routes.draw  do
+  get "carts/index"
+  get "carts/add"
   resources :toys
+
+
+root :to => "toys#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
